c9886c03794bb15ce35be7676039198f5233aa04
Henry Reeves <hreeves@islander.tamucc.edu>
2019-01-09 10:47:58 -0600

    Adding Data Entry - incomplete

---
+++ Library/DBHelper.php
@@ -0,0 +1,204 @@
+<?php
+//NOTE:
+/*
+- BEFORE EXECUTING QUERIES OR CALL SPs, MUST EITHER:
+    + Use prepare statement
+    + OR Sanitize input using mysqli_real_escape_string
+- BIND PARAMETER: Types: s = string, i = integer, d = double,  b = blob
+*/
+//PLEASE USE THIS TEMPLATE TO COMMENT ON FUNCTIONS
+/**********************************************
+Function:
+Description:
+Parameter(s):
+Return value(s):
+ ***********************************************/
+class DBHelper
+{
+    //Members
+    static protected $ini_dir = "dnd_website_config\\dnd_website.ini";
+    protected $host;
+    protected $user;
+    protected $pwd;
+    protected $maindb;
+    protected $conn;
+
+    //Getter and setters
+    function __construct()
+    {
+        $root = substr(getcwd(), 0, strpos(getcwd(), "htdocs\\")); //point to xampp// directory
+        $config = parse_ini_file($root . DBHelper::$ini_dir);
+        $this->host = $config['servername'];
+        $this->user = $config['username'];
+        $this->pwd = $config['password'];
+        $this->maindb = $config['dbname'];
+
+        /*if not currently connected, attempt to connect to DB*/
+        if ($this->getConn() == null)
+            $this->DB_CONNECT(null);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConn()
+    {
+        return $this->conn;
+    }
+
+    /**
+     * @param mixed $conn
+     */
+    public function setConn($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    /**********************************************
+     * Function: DB_CONNECT
+     * Description: Connect to XAMPP db. localhost/phpadmin All functions connect to db using this.
+     * Parameter(s):
+     * $db (string) - name of database to be connected to
+     * Return value(s):
+     * $conn (object) - return connection object if success
+     * - return -1 if failed
+     ***********************************************/
+    function DB_CONNECT($db)
+    {
+        if ($db == "" || $db == null) //empty parameter = default = bandocatdb
+            $db = "equipment";
+        /* assign conn as a PHP Data Object, concat the host, user and pwd */
+        $this->conn = new PDO('mysql:host=' . $this->getHost() . ';dbname=' . $db, $this->getUser(), $this->getPwd());
+        return 0;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+    //Constructor
+
+    /**
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+    //IMPORTANT: Default Host, username, password can be changed here!
+
+    /**
+     * @return string
+     */
+    public function getPwd()
+    {
+        return $this->pwd;
+    }
+
+    function DB_CLOSE()
+    {
+        $this->setConn(null);
+    }
+
+    public function getMysqliConnection()
+    {
+        return $db = new mysqli($this->host, $this->user, $this->pwd, $this->maindb);
+    }
+
+
+    /**********************************************
+     * Function: SWITCH_DB
+     * Description:This function switches the current connection to the database specified in the parameter
+     * Parameter(s):
+     *$collection (string) - db parameter name (such as bluchermaps, greenmaps)
+     * Return value(s): true if success, false if error occurs
+     ***********************************************/
+    public function SWITCH_DB($collection)
+    {
+        if ($collection == null || $collection == "") //or == maindb
+            $this->getConn()->exec('USE ' . self::$maindb);
+        //get appropriate database
+        $dbname = $this->SP_GET_COLLECTION_CONFIG(htmlspecialchars($collection))['DbName'];
+        if ($dbname != null && $dbname != "") {
+            $this->getConn()->exec('USE ' . $dbname);
+            return true;
+        }
+        return false;
+    }
+
+    /**********************************************
+     * Function: SP_GET_COLLECTION_CONFIG (SP_GET_COLLECTION_DATA)
+     * Description: Pulls the data associated with the collection name passed in.
+     * Parameter(s):
+     * $iName (in string) - input DB Name
+     * Return value(s):
+     * $result (assoc array) - return above values in an assoc array
+     ***********************************************/
+    function SP_GET_COLLECTION_CONFIG($iName)
+    {   //USE is sql for changing to the database supplied after the concat.
+        $this->getConn()->exec('USE ' . $this->maindb);
+        /* PREPARE STATEMENT */
+        //CALL is sql for telling the db to execute the function following call.
+        //The ? in the functions parameter list is a variable that we bind a few lines down
+        $call = $this->getConn()->prepare("CALL SP_GET_COLLECTION_CONFIG(?,@oDisplayName,@oDbName,@oStorageDir,@oPublicDir,@oThumbnailDir,@oTemplateID,@oGeorecDir,@oCollectionID)");
+        //ERROR HANDLING
+        if (!$call)
+            trigger_error("SQL failed: " . $this->getConn()->errorCode() . " - " . $this->conn->errorInfo()[0]);
+        //Bind the database name ($iName) into the prepared call statement from above.
+        $call->bindParam(1, $iName, PDO::PARAM_STR | PDO::PARAM_INPUT_OUTPUT, 50);
+        /* EXECUTE STATEMENT */
+        $call->execute();
+        /* RETURN RESULT */
+        //returned after successful call statement. Need to tell the db what data we want selected.
+        $select = $this->getConn()->query('SELECT @oDisplayName AS DisplayName,@oDbName AS DbName,@oStorageDir AS StorageDir,@oPublicDir AS PublicDir,@oThumbnailDir AS ThumbnailDir,@oTemplateID as TemplateID,@oGeorecDir as GeoRecDir,@oCollectionID as CollectionID');
+        //Database now has the appropriate data selected. We now need to ask the DB to fetch the values for us
+        $result = $select->fetch(PDO::FETCH_ASSOC);
+        $result["Name"] = htmlspecialchars($iName);
+        return $result;
+    }
+
+    function GET_ALL_TABLES()
+    {
+        $conn = $this->getMysqliConnection();
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SHOW TABLES";
+
+        $listdbtables = array_column(mysqli_fetch_all($conn->query('SHOW TABLES')),0);
+
+        $conn->close();
+        return $listdbtables;
+    }
+
+    function SELECT_ALL($table)
+    {
+        $conn = $this->getMysqliConnection($dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT * FROM $table";
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0)
+        {
+            // output data of each row, there is only one though
+            while($row = $result->fetch_assoc())
+            {
+                $data = $row;
+            }
+        }
+        $conn->close();
+        return $data;
+    }
+}
--- index.php
+++ index.php
@@ -5,3 +5,111 @@
  * Date: 1/9/2019
  * Time: 9:16 AM
  */
+require "./Library/DBHelper.php";
+$DB = new DBHelper();
+?>
+<!DOCTYPE html>
+<html>
+<title>Data Entry</title>
+<head>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <!-- data tables bootstrap style cdn -->
+    <link href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+<body>
+
+<!-- Whole Page -->
+<div class="container-fluid bg-primary">
+    <div class="container bg-white">
+        <h1>Data Entry</h1>
+        <hr>
+        <div class="row">
+            <div class="col">
+                Table <select id="ddlTable">
+                    <?php
+                    $tables = $DB->GET_ALL_TABLES();
+
+                    foreach($tables as $table)
+                    {
+                        echo "<option value='$table'>$table</option>";
+                    }
+                    ?></select>
+            </div>
+        </div>
+        <!-- Datatable -->
+        <div class="row">
+            <table id="example" class="table table-striped table-bordered" style="width:100%">
+                <thead>
+                <tr>
+                    
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
+
+<!-- Put script tags at the bottom of the page, makes the page load faster -->
+<!-- JQuery cdn -->
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+
+<!-- Bootstrap complete JavaScript -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+<!-- Datatables w/bootstrap -->
+<script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+
+<script>
+    $(document).ready(function() {
+        // Load the data table
+        $('#example').DataTable();
+    });
+
+    // WHen the table is changed, load the data table
+    $("#ddlTable").change(function(){
+        console.log("Hey");
+    });
+
+    // Used to show the datatable
+    function showTable() {
+        // Creating data table and defining its properties
+        var table = $('#dataTable').DataTable({
+            "processing": true,
+            "serverside": true,
+            "lengthMenu": [20, 40, 60, 80, 100],
+            "destroy": true,
+
+            // Displaying loading gif
+            "language": {
+                "processing": "<figure id='figLoad'>" +
+                "<img src='../../EmployeeManager/Forms/LoadingGifs/loading-gif-1.gif' style='width: 50%; height: 50%;'><figcaption>Loading</figcaption></figure>"
+            },
+
+            "initComplete": function () {
+                console.log("Table done loading...");
+            },
+
+            // Getting select statement
+            ajax:
+                {
+                    url: "../Master/Server_Scripts/ManageEmployeesManager.php?getDataTable=" + true,
+                },
+
+            columns: [
+                {data: 'id'},
+                {data: 'first_name'},
+                {data: 'last_name'},
+                {data: 'gender'},
+                {data: 'hire_date'},
+                {data: 'employee_number'},
+                {data: 'admin'},
+                {data: 'title'},
+                {data: 'street_address'}
+            ]
+        });
+    }
+</script>
+</body>
+</html>
